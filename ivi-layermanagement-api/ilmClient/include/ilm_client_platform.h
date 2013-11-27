159ff2c2fc9d77610c622f02953b49a2bf8bfb1e
Nobuhiko Tanibata <NOBUHIKO_TANIBATA@denso.co.jp>
2013-11-27 10:40:22 +0900

    ivi-layermanagement-api: ilmClient headers
    Signed-off-by: Nobuhiko Tanibata <NOBUHIKO_TANIBATA@denso.co.jp>

---
+++ ivi-layermanagement-api/ilmClient/include/ilm_client_platform.h
@@ -0,0 +1,58 @@
+/**************************************************************************
+ *
+ * Copyright (C) 2013 DENSO CORPORATION
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ****************************************************************************/
+#ifndef _ILM_CLIENT_PLATFORM_H_
+#define _ILM_CLIENT_PLATFORM_H_
+
+#ifdef __cplusplus
+
+extern "C" {
+#endif /* __cplusplus */
+
+#include "ilm_common.h"
+
+typedef struct _ILM_CLIENT_PLATFORM_FUNC
+{
+    ilmErrorTypes (*getScreenResolution)(t_ilm_uint screenID,
+                   t_ilm_uint* pWidth, t_ilm_uint* pHeight);
+    ilmErrorTypes (*surfaceAddNotification)(t_ilm_surface surface,
+                   surfaceNotificationFunc callback);
+    ilmErrorTypes (*surfaceCreate)(t_ilm_nativehandle nativehandle,
+                   t_ilm_int width, t_ilm_int height,
+                   ilmPixelFormat pixelFormat, t_ilm_surface* pSurfaceId);
+    ilmErrorTypes (*surfaceRemove)(const t_ilm_surface surfaceId);
+    ilmErrorTypes (*surfaceRemoveNativeContent)(t_ilm_surface surfaceId);
+    ilmErrorTypes (*surfaceRemoveNotification)(t_ilm_surface surface);
+    ilmErrorTypes (*surfaceSetNativeContent)(t_ilm_nativehandle nativehandle,
+                   t_ilm_int width, t_ilm_int height,
+                   ilmPixelFormat pixelFormat, t_ilm_surface surfaceId);
+    ilmErrorTypes (*UpdateInputEventAcceptanceOn)(t_ilm_surface surfaceId,
+                   ilmInputDevice devices, t_ilm_bool acceptance);
+    ilmErrorTypes (*init)(t_ilm_nativedisplay nativedisplay);
+    void (*destroy)();
+    ilmErrorTypes (*surfaceInitialize)(t_ilm_surface *pSurfaceId);
+} ILM_CLIENT_PLATFORM_FUNC;
+
+ILM_CLIENT_PLATFORM_FUNC gIlmClientPlatformFunc;
+
+void init_ilmClientPlatformTable();
+
+#ifdef __cplusplus
+} /**/
+#endif /* __cplusplus */
+
+#endif /* _ILM_CLIENT_PLATFORM_H_ */
