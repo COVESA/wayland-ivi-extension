593b7293282dec12f89a2404b8bcafb6cff6acfc
Lukasz Juszkiewicz <lukasz.juszkiewicz@bmw-carit.de>
2014-04-30 18:15:27 +0200

    tests: layer capabilities
    Signed-off-by: Lukasz Juszkiewicz <lukasz.juszkiewicz@bmw-carit.de>

--- ivi-layermanagement-api/test/ilm_control_test.cpp
+++ ivi-layermanagement-api/test/ilm_control_test.cpp
@@ -608,6 +608,48 @@ TEST_F(IlmCommandTest, ilm_layerGetType_InvalidInput) {
     ASSERT_EQ(ILM_LAYERTYPE_UNKNOWN, type);
 }
 
+TEST_F(IlmCommandTest, ilm_layerGetCapabilities) {
+    t_ilm_uint layer = 0xbeef;
+    t_ilm_layercapabilities caps;
+    // TODO: unsupported
+    t_ilm_layercapabilities exp_caps = 0;
+
+    // add a layer and check its capabilities
+    ASSERT_EQ(ILM_SUCCESS, ilm_layerCreateWithDimension(&layer, 800, 480));
+    ASSERT_EQ(ILM_SUCCESS, ilm_commitChanges());
+    ASSERT_EQ(ILM_SUCCESS, ilm_layerGetCapabilities(layer, &caps));
+    ASSERT_EQ(exp_caps, caps);
+
+    // cleanup
+    ASSERT_EQ(ILM_SUCCESS, ilm_layerRemove(0xbeef));
+}
+
+TEST_F(IlmCommandTest, ilm_layerTypeGetCapabilities) {
+    t_ilm_layercapabilities caps;
+    // TODO: unsupported
+    t_ilm_layercapabilities exp_caps = 0;
+
+    // check ILM_LAYERTYPE_UNKNOWN
+    exp_caps = 0;
+    ASSERT_EQ(ILM_SUCCESS, ilm_layerTypeGetCapabilities(ILM_LAYERTYPE_UNKNOWN, &caps));
+    ASSERT_EQ(exp_caps, caps);
+
+    // check ILM_LAYERTYPE_HARDWARE
+    exp_caps = 0;
+    ASSERT_EQ(ILM_SUCCESS, ilm_layerTypeGetCapabilities(ILM_LAYERTYPE_HARDWARE, &caps));
+    ASSERT_EQ(exp_caps, caps);
+
+    // check ILM_LAYERTYPE_SOFTWARE2D
+    exp_caps = 0;
+    ASSERT_EQ(ILM_SUCCESS, ilm_layerTypeGetCapabilities(ILM_LAYERTYPE_SOFTWARE2D, &caps));
+    ASSERT_EQ(exp_caps, caps);
+
+    // check ILM_LAYERTYPE_SOFTWARE2_5D
+    exp_caps = 1;
+    ASSERT_EQ(ILM_SUCCESS, ilm_layerTypeGetCapabilities(ILM_LAYERTYPE_SOFTWARE2_5D, &caps));
+    ASSERT_EQ(exp_caps, caps);
+}
+
 TEST_F(IlmCommandTest, ilm_surface_initialize) {
     uint surface_10 = 10;
     uint surface_20 = 20;
