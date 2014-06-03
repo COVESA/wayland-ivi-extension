772ddf68618336c87273855045df8d3dcedc71b7
Nobuhiko Tanibata <NOBUHIKO_TANIBATA@xddp.denso.co.jp>
2014-06-03 06:08:12 -0700

    test: Avoid surface creation failure.
    Signed-off-by: Nobuhiko Tanibata <NOBUHIKO_TANIBATA@denso.co.jp>

--- ivi-layermanagement-api/test/ilm_control_notification_test.cpp
+++ ivi-layermanagement-api/test/ilm_control_notification_test.cpp
@@ -187,7 +187,7 @@ TEST_F(NotificationTest, ilm_surfaceAddNotificationWithoutCallback)
     // create a layer
     t_ilm_uint surface = 67;
 
-    ilm_surfaceCreate((t_ilm_nativehandle)wlSurfaces[0],10,10,ILM_PIXELFORMAT_RGBA_8888,&surface);
+    ilm_surfaceCreate((t_ilm_nativehandle)wlSurfaces[1],10,10,ILM_PIXELFORMAT_RGBA_8888,&surface);
     ilm_commitChanges();
 
     // add notification
