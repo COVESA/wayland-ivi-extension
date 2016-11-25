ba20fb28aba6706c039159940f7dfc67e7455ac8
Emre Ucan <eucan@de.adit-jv.com>
2016-11-25 12:32:41 +0100

    LayerManagerControl: remove demo command
    It is not working correctly and it is not needed.
    
    Signed-off-by: Emre Ucan <eucan@de.adit-jv.com>

--- ivi-layermanagement-examples/LayerManagerControl/include/LMControl.h
+++ ivi-layermanagement-examples/LayerManagerControl/include/LMControl.h
@@ -296,18 +296,6 @@ void scatterAll();
  */
 t_scene_data getScatteredScene(t_scene_data* pInitialScene);
 
-
-//=============================================================================
-//demo.cpp
-//=============================================================================
-
-/*
- * Run a set of effects on the rendered scene depending on the parameter.
- * The function accepts values in range 0-4
- */
-void demo(t_ilm_uint mode);
-
-
 //=============================================================================
 //sceneio.cpp
 //=============================================================================
