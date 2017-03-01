45d0e1dd50bf60e18f574888d75242ab66d71bf4
Emre Ucan <eucan@de.adit-jv.com>
2017-03-01 13:26:21 +0100

    ivi-input: fix white space errors in set_weston_focus
    I fixed the mixed usage of spaces and tabs to only using
    spaces.
    
    Signed-off-by: Emre Ucan <eucan@de.adit-jv.com>

--- ivi-input-modules/ivi-input-controller/src/ivi-input-controller.c
+++ ivi-input-modules/ivi-input-controller/src/ivi-input-controller.c
@@ -181,42 +181,39 @@ set_weston_focus(struct input_context *ctx, struct surface_ctx *surface_ctx,
               ctx->ivi_layout_interface;
 
     /* Assume one view per surface */
-	w_surface = interface->surface_get_weston_surface(surface_ctx->layout_surface);
+    w_surface = interface->surface_get_weston_surface(surface_ctx->layout_surface);
     view = wl_container_of(w_surface->views.next, view, surface_link);
-	if ((focus & ILM_INPUT_DEVICE_POINTER) &&
-		 (pointer != NULL)){
-		if ( (view != NULL) && enabled) {
-	        weston_view_to_global_fixed(view, wl_fixed_from_int(0),
-	            wl_fixed_from_int(0), &x, &y);
-	        // move pointer to local (0,0) of the view
-	        pointer_move(pointer, x, y);
-		    weston_pointer_set_focus(pointer, view,
-		        wl_fixed_from_int(0), wl_fixed_from_int(0));
-		} else if (pointer->focus == view){
+    if ((focus & ILM_INPUT_DEVICE_POINTER) && (pointer != NULL)){
+        if ( (view != NULL) && enabled) {
+            weston_view_to_global_fixed(view, wl_fixed_from_int(0),
+            wl_fixed_from_int(0), &x, &y);
+            // move pointer to local (0,0) of the view
+            pointer_move(pointer, x, y);
+            weston_pointer_set_focus(pointer, view,
+            wl_fixed_from_int(0), wl_fixed_from_int(0));
+        } else if (pointer->focus == view) {
             weston_pointer_set_focus(pointer, NULL,
-                wl_fixed_from_int(0), wl_fixed_from_int(0));
-		}
-	}
+            wl_fixed_from_int(0), wl_fixed_from_int(0));
+        }
+    }
 
-	if ((focus & ILM_INPUT_DEVICE_KEYBOARD) &&
-		 (keyboard != NULL)) {
-		if (w_surface) {
-	        surface_client = wl_resource_get_client(w_surface->resource);
-	        serial = wl_display_next_serial(ctx->compositor->wl_display);
-	        wl_resource_for_each(resource, &keyboard->resource_list) {
-	            if (wl_resource_get_client(resource) != surface_client)
-	                continue;
+    if ((focus & ILM_INPUT_DEVICE_KEYBOARD) && (keyboard != NULL)) {
+        if (w_surface) {
+            surface_client = wl_resource_get_client(w_surface->resource);
+            serial = wl_display_next_serial(ctx->compositor->wl_display);
+            wl_resource_for_each(resource, &keyboard->resource_list) {
+                if (wl_resource_get_client(resource) != surface_client)
+                    continue;
 
-	            if (!enabled) {
-	                wl_keyboard_send_leave(resource, serial,
-	                         w_surface->resource);
-	            } else {
+                if (!enabled) {
+                    wl_keyboard_send_leave(resource, serial, w_surface->resource);
+                } else {
                     wl_keyboard_send_enter(resource, serial, w_surface->resource,
                             &keyboard->keys);
-	            }
-	        }
-		}
-	}
+                }
+            }
+        }
+    }
 }
 
 static void
