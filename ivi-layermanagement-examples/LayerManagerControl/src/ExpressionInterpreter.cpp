56964fe775348d1aecd1a67e951e25b82ea78a03
Rajendraprasad K J <KarammelJayakumar.Rajendraprasad@in.bosch.com>
2019-04-02 01:46:45 -0400

    LayerManagerControl: Prevent conversion of filenames into lowercase
    LayerManagerControl auto-convert all text expressions to lowercase which includes
    the dump filenames that the user provides.
    LayerManagerControl should use the filename as provided and should not modify it.
    Added necessary changes in LayerMangerControl source code so that the text is not
    converted to lowercase if the expression is a filename.
    
    Signed-off-by: Rajendraprasad K J <KarammelJayakumar.Rajendraprasad@in.bosch.com>

--- ivi-layermanagement-examples/LayerManagerControl/src/ExpressionInterpreter.cpp
+++ ivi-layermanagement-examples/LayerManagerControl/src/ExpressionInterpreter.cpp
@@ -83,7 +83,6 @@ CommandResult ExpressionInterpreter::interpretCommand(string userInput)
     while (result == CommandSuccess && !ss.eof())
     {
         ss >> text;
-        transform(text.begin(), text.end(), text.begin(), ::tolower);
 
         ExpressionList::const_iterator iter = currentState.begin();
         ExpressionList::const_iterator end = currentState.end();
